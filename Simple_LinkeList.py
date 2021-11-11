e046bd326e7c0a90d19a468ef277947a574efe09
Kashit <75305061+HeyKashit@users.noreply.github.com>
2021-11-11 23:00:03 +0530

    Update Simple_LinkeList.py

--- Simple_LinkeList.py
+++ Simple_LinkeList.py
@@ -1,23 +1,66 @@
-Class Node:
-	def __init__(self,k):
-		self.key=k
+class Node:
+	def __init__(self,value):
+		self.key=value
 		self.next=None
 
-# print linked list function.
-def print_list(head):
-	curr = head
-	while (curr != None):
-		print(curr.key, end=" ")
-		curr= curr.next
+def print_linked_list(head):
+	curr=head
+	while(curr!=None):
+		print(curr.key,end=" ")
+		curr=curr.next
+      
+def search(head,x):
+	pos=0
+	curr=head
+	while(curr!=None):
+		if curr.key==x:
+			return pos
+		pos=pos+1
+		curr=curr.next
+	return -1
+
+def insert_at_head(head,x):
+	temp=Node(x)
+	temp.next=head
+	return temp
 
 
-# Driver code.
-temp1 = Node(10)
-temp2 = Node(20)
-temp3 = Node(30)
+def inster_at_end(head,x):
+	curr=head
+	if head == None:
+		return Node(x)
+	while(curr.next!=None):
+		curr=curr.next
+	curr.next=Node(x)
+	return head
 
-temp1.next =temp2
-temp2.next =temp3
-head=temp1
-
-print_list(head)
+# insert at any position
+def at_any_position(head,pos,x):
+	if pos==1:
+		temp=Node(x)
+		temp.next=head
+		return temp
+	curr=head
+	for i in range (pos-2):
+		curr=curr.next
+		if curr == None:
+			return head
+	temp=Node(x)
+	temp.next = curr.next
+	curr.next=temp
+	return head
+# delete at the front
+def delet_front(head):
+	pass
+# Driver code
+head=Node(10)
+head=insert_at_head(head,20)
+head=insert_at_head(head,100)
+head=insert_at_head(head,60)
+head=insert_at_head(head,30)
+head=inster_at_end(head,9999)
+head=at_any_position(head,2,88888888)
+print_linked_list(head)
+print("  search ")	
+ans=search(head,10)	
+print(ans)
