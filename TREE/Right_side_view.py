fa7f5d1a1a311ef6809bb5694dba116b209e957f
Kashit <75305061+HeyKashit@users.noreply.github.com>
2022-08-05 00:53:24 +0530

    Create Right_side_view.py

---
+++ TREE/Right_side_view.py
@@ -0,0 +1,11 @@
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        x=defaultdict(list)
+        def r_side(node,h):
+            if not node: return 
+            x[h].append(node.val)
+            r_side(node.left,h+1)
+            r_side(node.right,h+1)
+            
+        r_side(root,0)
+        
+        return [v[-1] for k,v in sorted(x.items())]
