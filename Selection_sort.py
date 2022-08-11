fbf0c48c04978a82e7ec43110104e3b827c81d22
Kashit <75305061+HeyKashit@users.noreply.github.com>
2022-08-12 02:29:35 +0530

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -6,3 +6,16 @@ def selectionSort(customList):
                 min_index = j
         customList[i], customList[min_index] = customList[min_index], customList[i]
     print(customList)
+    
+    
+A = ['t','u','t','o','r','i','a','l']
+for i in range(len(A)):
+   min_= i
+   for j in range(i+1, len(A)):
+      if A[min_] > A[j]:
+         min_ = j
+   #swap
+A[i], A[min_] = A[min_], A[i]
+# main
+for i in range(len(A)):
+   print(A[i])
